8586260c27932b592a80798789d279c3ed8d45be
Kevin Lambert <kevinslambert@hotmail.com>
2015-02-17 20:18:02 -0700

    Initial Commit

--- No Stupid Questions Alpha/AppDelegate.m
+++ No Stupid Questions Alpha/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -16,7 +17,16 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    // [Optional] Power your app with Local Datastore. For more info, go to
+    // https://parse.com/docs/ios_guide#localdatastore/iOS
+    [Parse enableLocalDatastore];
+    
+    // Initialize Parse.
+    [Parse setApplicationId:@"gGrzVgO7qidn0cBro3y0AYzQOwUE2tECsYk53OrP"
+                  clientKey:@"RlQOdZhozxNYGYzWFq4O43Zgzgok0XvScVL3LkFh"];
+    
+    // [Optional] Track statistics around application opens.
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     return YES;
 }
 
--- No Stupid Questions Alpha/ViewController.m
+++ No Stupid Questions Alpha/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Parse/Parse.h>
 
 @interface ViewController ()
 
@@ -16,6 +17,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+//    PFObject *testObject = [PFObject objectWithClassName:@"Teacher"];
+//    testObject[@"Lessons"] = @"Site Tablet";
+//    [testObject saveInBackground];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
