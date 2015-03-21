ee6e3c907e733640e699d80e72516536c7655a1d
Kevin Lambert <kevinslambert@hotmail.com>
2015-03-21 01:02:05 -0600

    On last view controller

---
+++ No Stupid Questions Alpha/TeacherLandingPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  TeacherLandingPageViewController.h
+//  No Stupid Questions Alpha
+//
+//  Created by Kevin Lambert on 3/20/15.
+//  Copyright (c) 2015 KSL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TeacherLandingPageViewController : UIViewController
+
+@end
